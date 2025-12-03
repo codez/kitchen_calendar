5d60f238ddf1f16a50f9b608f24d7261abeac175
Pascal Zumkehr <spam@codez.ch>
2025-12-03 22:15:44 +0100

    Refactor to classes and single page a3 format

--- holidays.rb
+++ holidays.rb
@@ -1,61 +1,65 @@
+# frozen_string_literal: true
+
 # Constants to configure holidays that
 # will be calculated for a given year.
 
-HOLIDAYS_FIXED = [
-  [1, 1, "Neujahr"],
-  [1, 2, "Berchtoldstag"],
-  [8, 1, "Bundesfeiertag"],
-  [12, 25, "Weihnachten"],
-  [12, 26, "Stefanstag"]
-]
+class Holidays
+  HOLIDAYS_FIXED = [
+    [1, 1, 'Neujahr'],
+    [1, 2, 'Berchtoldstag'],
+    [8, 1, 'Bundesfeiertag'],
+    [12, 25, 'Weihnachten'],
+    [12, 26, 'Stefanstag']
+  ].freeze
 
-HOLIDAYS_RELATIVE_TO_EASTER = [
-  [-2, "Karfreitag"],
-  [0, "Ostersonntag"],
-  [1, "Ostermontag"],
-  [39, "Auffahrt"],
-  [49, "Pfingstsonntag"],
-  [50, "Pfingstmontag"]
-]
+  HOLIDAYS_RELATIVE_TO_EASTER = [
+    [-2, 'Karfreitag'],
+    [0, 'Ostersonntag'],
+    [1, 'Ostermontag'],
+    [39, 'Auffahrt'],
+    [49, 'Pfingstsonntag'],
+    [50, 'Pfingstmontag']
+  ].freeze
 
-def holiday_dates(year)
-  holidays_fixed(year).merge(holidays_relative_to_easter(year))
-end
-
-private
-
-def holidays_fixed(year)
-  HOLIDAYS_FIXED.each_with_object({}) do |(month, day, name), hash|
-    hash[Date.new(year, month, day)] = name
+  def dates(year)
+    fixed(year).merge(relative_to_easter(year))
   end
-end
 
-def holidays_relative_to_easter(year)
-  easter = easter_in(year)
-  HOLIDAYS_RELATIVE_TO_EASTER.each_with_object({}) do |(offset, name), hash|
-    hash[easter + offset] = name
+  private
+
+  def fixed(year)
+    HOLIDAYS_FIXED.each_with_object({}) do |(month, day, name), hash|
+      hash[Date.new(year, month, day)] = name
+    end
   end
-end
 
-# Copyright by holidays gem
-# https://github.com/holidays/holidays/blob/master/lib/holidays/date_calculator/easter.rb
-def easter_in(year)
-  y = year
-  a = y % 19
-  b = y / 100
-  c = y % 100
-  d = b / 4
-  e = b % 4
-  f = (b + 8) / 25
-  g = (b - f + 1) / 3
-  h = (19 * a + b - d - g + 15) % 30
-  i = c / 4
-  k = c % 4
-  l = (32 + 2 * e + 2 * i - h - k) % 7
-  m = (a + 11 * h + 22 * l) / 451
+  def relative_to_easter(year)
+    easter = easter_in(year)
+    HOLIDAYS_RELATIVE_TO_EASTER.each_with_object({}) do |(offset, name), hash|
+      hash[easter + offset] = name
+    end
+  end
 
-  month = (h + l - 7 * m + 114) / 31
-  day = ((h + l - 7 * m + 114) % 31) + 1
+  # Copyright by holidays gem
+  # https://github.com/holidays/holidays/blob/master/lib/holidays/date_calculator/easter.rb
+  def easter_in(year)
+    y = year
+    a = y % 19
+    b = y / 100
+    c = y % 100
+    d = b / 4
+    e = b % 4
+    f = (b + 8) / 25
+    g = (b - f + 1) / 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c / 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    m = (a + 11 * h + 22 * l) / 451
 
-  Date.new(year, month, day)
+    month = (h + l - 7 * m + 114) / 31
+    day = ((h + l - 7 * m + 114) % 31) + 1
+
+    Date.new(year, month, day)
+  end
 end
--- kitchen_calendar.rb
+++ kitchen_calendar.rb
@@ -10,145 +10,223 @@ require 'prawn'
 require 'prawn/measurement_extensions'
 require_relative 'holidays'
 
-year = (ARGV.last || Date.today.year).to_i
-a3 = ARGV.first == '--a3'
-holidays = holiday_dates(year)
-filename = "calendar-#{year}#{a3 ? '-a3' : ''}.pdf"
-font = Dir.glob('./*.ttf').first || 'Helvetica'
+class KitchenCalendar
+  include Prawn::View
 
-# Constants to configure layout and font
+  COLOR = '2a3773'
+  WHITE = 'FFFFFF'
+  LINE_WIDTH = 1.5
 
-if a3
-  PAGE_SIZE = [420.mm, 297.mm].freeze
-  MARGIN = [12.mm, 3.mm, 153.mm, 3.mm]
-  COLUMN_WIDTH = 26.mm
-  PADDING_BOX = 2.mm
-  PADDING_TEXT = 3.mm
-  COLUMN_HEIGHT = 98.mm
-else
-  PAGE_SIZE = [900.mm, 162.mm].freeze
-  MARGIN = 12.mm
-  COLUMN_WIDTH = 28.mm
-  PADDING_BOX = 3.mm
-  PADDING_TEXT = 4.mm
-  COLUMN_HEIGHT = 105.mm
+  MONTH_NAMES = %w[
+    null
+    Januar
+    Februar
+    März
+    April
+    Mai
+    Juni
+    Juli
+    August
+    September
+    Oktober
+    November
+    Dezember
+  ].freeze
+
+  WEEKDAY_NAMES = %w[So Mo Di Mi Do Fr Sa].freeze
+
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def generate_pdf
+    render
+    save_as(filename)
+  end
+
+  def filename = "calendar-#{year}.pdf"
+
+  private
+
+  def document
+    @document ||= Prawn::Document.new(page_size:, margin:)
+  end
+
+  def render
+    setup
+    (1..12).each do |month|
+      render_month(month)
+    end
+  end
+
+  def setup
+    font(font_name, style: font_style)
+    stroke_color(COLOR)
+    fill_color(COLOR)
+    line_width(LINE_WIDTH)
+  end
+
+  def render_month(month)
+    start_new_page if month > 1
+    render_title(month)
+    render_dates(month_dates(month))
+  end
+
+  def render_title(month)
+    text_box(MONTH_NAMES.fetch(month), at: [0, title_top], size: font_size_title)
+  end
+
+  def month_dates(month)
+    (Date.new(year, month, 1)..Date.new(year, month, -1)).to_a
+  end
+
+  def render_dates(dates)
+    render_vertical_line(0)
+    dates.each do |date|
+      render_date(date)
+    end
+  end
+
+  def render_date(date)
+    column_left = date_offset(date) * column_width
+    text_left = column_left + padding_text
+
+    render_vertical_line(column_left + column_width)
+    render_weekend_background(date, column_left)
+    render_weekday_label(date, text_left)
+    render_date_label(date, text_left)
+    render_holiday_name(date, text_left)
+    render_znacht(text_left)
+  end
+
+  def date_offset(date)
+    date.day - 1
+  end
+
+  def render_vertical_line(x)
+    stroke_vertical_line(column_height, 0, at: x)
+  end
+
+  def render_weekend_background(date, column_left)
+    return unless [0, 6].include?(date.wday)
+
+    fill_rectangle([column_left + padding_box, column_height],
+                   weekend_bg_width,
+                   weekend_bg_height)
+    fill_color(WHITE) # for weekday label
+  end
+
+  def render_weekday_label(date, text_left)
+    text_box(WEEKDAY_NAMES.fetch(date.wday),
+             at: [text_left, weekday_top],
+             size: font_size_wday)
+  end
+
+  def render_date_label(date, text_left)
+    fill_color(COLOR) # change back
+    text_box("#{date.day}.#{date.month}.",
+             at: [text_left, date_top],
+             size: font_size_date)
+  end
+
+  def render_holiday_name(date, text_left)
+    return unless holidays.key?(date)
+
+    text_box(holidays[date],
+             at: [text_left, holiday_top],
+             size: font_size_holiday,
+             width: holiday_width)
+  end
+
+  def render_znacht(text_left)
+    text_box('Z:',
+             at: [text_left, z_top],
+             size: font_size_content)
+  end
+
+  def holidays
+    @holidays ||= Holidays.new.dates(year)
+  end
+
+  def font_name = Dir.glob('./*.ttf').first || 'Helvetica'
+  def font_style = :bold
+  def font_size_title = 40
+  def font_size_wday = 20
+  def font_size_date = 18
+  def font_size_content = 12
+  def font_size_holiday = 9
+  def leading = 4
+  def page_size = [900.mm, 162.mm].freeze
+  def margin = 12.mm
+  def column_width = 28.mm
+  def column_height = 105.mm
+  def padding_text = 4.mm
+  def padding_box = 3.mm
+  def title_top = column_height + 28.mm
+  def weekend_bg_width = column_width - padding_box
+  def weekend_bg_height = font_size_wday + leading
+  def weekday_top = column_height - leading
+  def date_top = weekday_top - leading - font_size_wday
+  def holiday_top = date_top - leading - font_size_date
+  def holiday_width = column_width - padding_text
+  def z_top = 20.mm
 end
 
-FONT_SIZE_TITLE = 40
-FONT_SIZE_WDAY = 20
-FONT_SIZE_DATE = 18
-FONT_SIZE_CONTENT = 12
-FONT_SIZE_HOLIDAY = 9
-LEADING = 4
+class KitchenCalendarA3 < KitchenCalendar
+  MIDDLE_OF_MONTH = 16
 
-LINE_WIDTH = 1.5
-TITLE_TOP = COLUMN_HEIGHT + 28.mm
+  def filename = "calendar-#{year}-a3.pdf"
 
-WEEKDAY_TOP = COLUMN_HEIGHT - LEADING
-DATE_TOP = WEEKDAY_TOP - LEADING - FONT_SIZE_WDAY
-HOLIDAY_TOP = DATE_TOP - LEADING - FONT_SIZE_DATE
-Z_TOP = 20.mm
+  private
 
-COLOR = '2a3773'
-WHITE = 'FFFFFF'
+  def page_size = [420.mm, 297.mm].freeze
+  def margin = 3.mm
+  def font_size_holiday = 8.5
+  def column_width = 26.mm
+  def column_height = 98.mm
+  def padding_text = 3.mm
+  def padding_box = 2.mm
+  def middle = page_size[1] / 2.0 - margin
+  def bounds_width = page_size[0] - 2 * margin
+  def bounds_height = column_height + 9.mm
+  def title_top = middle + margin + bounds_height + 24.mm
 
-MONTH_NAMES = %w[
-  null
-  Januar
-  Februar
-  März
-  April
-  Mai
-  Juni
-  Juli
-  August
-  September
-  Oktober
-  November
-  Dezember
-].freeze
-
-WEEKDAY_NAMES = %w[
-  So
-  Mo
-  Di
-  Mi
-  Do
-  Fr
-  Sa
-].freeze
-
-MIDDLE_OF_MONTH = 17
-
-Prawn::Document.generate(filename, page_size: PAGE_SIZE, margin: MARGIN) do
-  font(font)
-  stroke_color(COLOR)
-  fill_color(COLOR)
-  line_width(LINE_WIDTH)
-
-  (1..12).each do |month|
-    start_new_page if month > 1
-    if a3
-      mask(:line_width) do # cut marker
-        line_width(0.5)
-        stroke_horizontal_line(0, 5.mm, at: -15.mm)
-        stroke_horizontal_line(409.mm, 414.mm, at: -15.mm)
-      end
+  def render_dates(dates)
+    render_half_month(middle + margin) do
+      super(dates[0...MIDDLE_OF_MONTH])
     end
+    render_half_month(0) do
+      super(dates[MIDDLE_OF_MONTH..])
+    end
+    render_cut_marker
+  end
 
-    # month title
-    text_box(MONTH_NAMES.fetch(month), at: [0, TITLE_TOP], size: FONT_SIZE_TITLE)
+  def render_half_month(offset, &)
+    bounding_box([0, offset + bounds_height], width: bounds_width, height: column_height, &)
+  end
 
-    stroke_vertical_line(COLUMN_HEIGHT, 0, at: 0)
-    date = Date.new(year, month, 1)
-    while date.month == month
-      if a3 && date.day == MIDDLE_OF_MONTH
-        start_new_page
-        stroke_vertical_line(COLUMN_HEIGHT, 0, at: 0)
-        mask(:line_width) do # cut marker
-          line_width(0.5)
-          stroke_horizontal_line(0, 5.mm, at: -15.mm)
-          stroke_horizontal_line(409.mm, 414.mm, at: -15.mm)
-        end
-      end
+  def render_cut_marker
+    mask(:line_width) do
+      line_width(0.5)
+      stroke_horizontal_line(0, 5.mm, at: middle)
+      stroke_horizontal_line(bounds_width - 5.mm, bounds_width, at: middle)
+    end
+  end
 
-      offset = date.day - 1
-      offset -= MIDDLE_OF_MONTH - 1 if a3 && date.day >= MIDDLE_OF_MONTH
-      column_left = offset * COLUMN_WIDTH
-      text_left = column_left + PADDING_TEXT
-      stroke_vertical_line(COLUMN_HEIGHT, 0, at: column_left + COLUMN_WIDTH)
-
-      # weekend background
-      if [0, 6].include?(date.wday)
-        fill_rectangle([column_left + PADDING_BOX, COLUMN_HEIGHT],
-                       COLUMN_WIDTH - PADDING_BOX,
-                       FONT_SIZE_WDAY + LEADING)
-        fill_color(WHITE) # for weekday label
-      end
-      # weekday label
-      text_box(WEEKDAY_NAMES.fetch(date.wday),
-               at: [text_left, WEEKDAY_TOP],
-               size: FONT_SIZE_WDAY)
-      # date label
-      fill_color(COLOR) # change back
-      text_box("#{date.day}.#{date.month}.",
-               at: [text_left, DATE_TOP],
-               size: FONT_SIZE_DATE)
-      # holiday name
-      if holidays.key?(date)
-        text_box(holidays[date],
-                 at: [text_left, HOLIDAY_TOP],
-                 size: FONT_SIZE_HOLIDAY,
-                 width: COLUMN_WIDTH - PADDING_TEXT)
-      end
-      # Z: (für Znacht)
-      text_box('Z:',
-               at: [text_left, Z_TOP],
-               size: FONT_SIZE_CONTENT)
-
-      date = date.next
+  def date_offset(date)
+    if date.day > MIDDLE_OF_MONTH
+      super - MIDDLE_OF_MONTH
+    else
+      super
     end
   end
 end
 
-`open #{filename} &`
+year = (ARGV.last || Date.today.year).to_i
+a3 = ARGV.first == '--a3'
+
+calendar = a3 ? KitchenCalendarA3.new(year) : KitchenCalendar.new(year)
+calendar.generate_pdf
+`open #{calendar.filename} &`
